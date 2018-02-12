7e75b3db4f2c1471e44ac11b0ef3f60ce49e90dd
comp500 <comp500@users.noreply.github.com>
2018-02-12 16:16:48 +0000

    Add ready check and login

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const playerID = "";
 const discordID = "";
 
 const request = require("request");
-const client = require("discord-rich-presence")(discordID);
 const url = `https://wiimmfi.de/mkw/list/${playerID}?m=json`;
 const DiscordRPC = require("discord-rpc");
 const rpc = new DiscordRPC.Client({
 	transport: 'ipc'
 });
 
-rpc.setActivity({
-	details: `9000 battle points`,
-	state: `Game mode`,
-	startTimestamp: Date.now(),
-	endTimestamp: Date.now() + 10,
-	largeImageKey: 'test_falcon',
-	largeImageText: "Moo Moo Meadows",
-	smallImageKey: 'test_falcon',
-	smallImageText: "Worldwide",
-	instance: false,
-});
+rpc.login(discordID).catch(console.error);
+
+rpc.on('ready', () => {
+	console.log(`Connected to discord with id ${discordID}`);
+	rpc.setActivity({
+		details: `9000 battle points`,
+		state: `6000 versus points`,
+		startTimestamp: Date.now(),
+		endTimestamp: Date.now() + 10,
+		largeImageKey: 'test_falcon',
+		largeImageText: "Moo Moo Meadows",
+		smallImageKey: 'test_falcon',
+		smallImageText: "Worldwide",
+		instance: false,
+	});
+});// fc, ev, eb, names, 
+// rk,
 
 /*request(url, (data) => {
 
